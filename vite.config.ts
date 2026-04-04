c8f28d877914a9bb68fa8dde91b8d3677ec4a7e7
Nahiyan-Al <alnahiyan2027@u.northwestern.edu>
2026-04-04 11:32:28 -0500

    Refactor to react-challenge template

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://github.com/reactjs/react.dev/issues/8353
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    tailwindcss(),
+    react(),
+    babel({
+      plugins: ['babel-plugin-react-compiler'],
+    }),
+  ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './vite-setup.ts',
+  },
 })
